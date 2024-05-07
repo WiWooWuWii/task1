25816993521133bb67fd6025b6e0fe717c73e76f
wiwoowuwii <94783226+WiWooWuWii@users.noreply.github.com>
2024-05-07 04:31:08 +0300

    Add files via upload

---
+++ ChatCommand.java
@@ -0,0 +1,7 @@
+package task19;
+
+public enum ChatCommand {
+    MAP,
+    RECYCLABILITY,
+    BONUS
+}
---
+++ MaterialType.java
@@ -0,0 +1,9 @@
+package task19;
+
+public enum MaterialType {
+    PLASTIC,
+    METAL,
+    GLASS,
+    PAPER,
+    UNSORTED_WASTE
+}
